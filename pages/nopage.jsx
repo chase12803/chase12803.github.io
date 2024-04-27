4ac94b4a8d7fd3ca71b55c83dbeab8beade92763
Chase Carstensen <chase.carstensen@outlook.com>
2024-04-27 15:25:51 -0400

    Cleaned error page

--- pages/nopage.jsx
+++ pages/nopage.jsx
@@ -2,7 +2,10 @@
 
 const Error = () => {
     return <div>
-      <section className="flex text-center font-bold text-green-600 bg-slate-800">
+      <section className="flex
+                          justify-center m-auto items-center
+                          font-bold text-blue-300 bg-slate-800 text-3xl
+                          h-screen">
         Error 404: Page not found
       </section>
     </div>
