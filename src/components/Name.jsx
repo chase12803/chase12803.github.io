435b9fbac75a24e827aaf4472567633b2397cfa1
Chase Carstensen <chase.carstensen@outlook.com>
2024-04-28 21:32:15 -0400

    WIP message

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -2,24 +2,44 @@ import { motion } from "framer-motion"
 import { evaluate } from "mathjs"
 
 const firstArr = [
-    { id: 1, letter: "C" },
-    { id: 2, letter: "h" },
-    { id: 3, letter: "a" },
-    { id: 4, letter: "s" },
-    { id: 5, letter: "e" },
+    { id: 1, letter: "W" },
+    { id: 2, letter: "o" },
+    { id: 3, letter: "r" },
+    { id: 4, letter: "k" },
+    { id: 5, letter: " " },
+    { id: 6, letter: "i" },
+    { id: 7, letter: "n" },
 ]
 const lastArr = [
-    { id: 6, letter: "C" },
-    { id: 7, letter: "a" },
-    { id: 8, letter: "r" },
-    { id: 9, letter: "s" },
-    { id: 10, letter: "t" },
-    { id: 11, letter: "e" },
-    { id: 12, letter: "n" },
-    { id: 13, letter: "s" },
-    { id: 14, letter: "e" },
-    { id: 15, letter: "n" },
+    { id: 8, letter: "p" },
+    { id: 9, letter: "r" },
+    { id: 10, letter: "o" },
+    { id: 11, letter: "g" },
+    { id: 12, letter: "r" },
+    { id: 13, letter: "e" },
+    { id: 14, letter: "s" },
+    { id: 15, letter: "s" },
 ]
+
+// const firstArr = [
+//     { id: 1, letter: "C" },
+//     { id: 2, letter: "h" },
+//     { id: 3, letter: "a" },
+//     { id: 4, letter: "s" },
+//     { id: 5, letter: "e" },
+// ]
+// const lastArr = [
+//     { id: 6, letter: "C" },
+//     { id: 7, letter: "a" },
+//     { id: 8, letter: "r" },
+//     { id: 9, letter: "s" },
+//     { id: 10, letter: "t" },
+//     { id: 11, letter: "e" },
+//     { id: 12, letter: "n" },
+//     { id: 13, letter: "s" },
+//     { id: 14, letter: "e" },
+//     { id: 15, letter: "n" },
+// ]
 
 const letterAnimation = {
     x: [evaluate('250*cos(90 deg)-250'), evaluate('250 * cos(75 deg)-250'), evaluate('250 * cos(60 deg)-250'), evaluate('250 * cos(45 deg)-250'), evaluate('250 * cos(30 deg)-250'), evaluate('250 * cos(15 deg)-250'), evaluate('250 * cos(0 deg)-250')],
